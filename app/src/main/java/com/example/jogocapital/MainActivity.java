e6220faa209fde8b49cea35987b43cccc56f7e0a
Enzo-Radel <enzo@radel.com.br>
2022-02-14 10:52:54 -0300

    Insersão estados e capitais

--- app/src/main/java/com/example/jogocapital/MainActivity.java
+++ app/src/main/java/com/example/jogocapital/MainActivity.java
@@ -6,9 +6,21 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String[] estados = {
+        "acre", "amapá", "bahia", "distrito federal", "goiás",
+        "mato grosso", "minas gerais", "paraíba", "pernambuco", "rio de janeiro",
+        "rio grande do sul", "roraima", "são paulo", "tocantins", "rio grande do norte"
+    };
+    private String[] capitais = {
+        "rio branco", "macapá", "salvador", "brasília", "goiânia",
+        "cuiabá", "belo horizonte", "joão pessoa", "recife", "rio de janeiro",
+        "porto alegre", "boa vista", "são paulo", "palmas", "natal"
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
