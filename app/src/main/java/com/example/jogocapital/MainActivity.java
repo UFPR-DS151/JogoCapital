c371105b34c3acb1dcac5bc835fe001faf2c1324
Enzo-Radel <enzo@radel.com.br>
2022-02-14 11:55:47 -0300

    gerando estado aleatório e validando resposta

--- app/src/main/java/com/example/jogocapital/MainActivity.java
+++ app/src/main/java/com/example/jogocapital/MainActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
+    // Estados e capitais ordenados através do índice
     private String[] estados = {
         "acre", "amapá", "bahia", "distrito federal", "goiás",
         "mato grosso", "minas gerais", "paraíba", "pernambuco", "rio de janeiro",
@@ -17,10 +22,62 @@ public class MainActivity extends AppCompatActivity {
         "porto alegre", "boa vista", "são paulo", "palmas", "natal"
     };
 
+    private String estadoAtual; // Estado que sistema gerou para a pergunta atual
+    private String capitalAtual; // Capital que é a resposta para o estado mostrado
+    private int pontos; // Pontos que o usuário marcou
+    private int rodada; // Rodada atual do jogo (máximo de 5)
+    private ArrayList<Integer> indexBlacklist; // array com os index correspondentes aos estados já mostrados
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public MainActivity() {
+        // Valores iniciais para os atributos do controller
+        this.estadoAtual = "";
+        this.capitalAtual = "";
+        this.pontos = 0;
+        this.rodada = 0;
+    }
+
+    public void randomState() {
+        // Chamar essa função quando o programa for iniciado (ou caso queira criar um botão "começar jogo", colocar após essa ação)
+        // e quando o usuário clicar em próxima pergunta
+        Random r = new Random();
+        do { // loop para repetir caso o programa gere um index já chamado anteriormente
+            int index = r.nextInt(15);
+            if (!this.indexBlacklist.contains(index)) {
+                this.estadoAtual = estados[index];
+                this.capitalAtual = capitais[index];
+                this.indexBlacklist.add(index);
+                break;
+            } else {
+                continue;
+            }
+        } while(true);
+
+        // Faltou fazer TextView que mostra o estado para o usuário receber o estado gerado
+    }
+
+    public void Answer() {
+        // Pegar valor da PlainText e inserir na váriavel "capitalEscolhida" (usando valor estático no momento)
+        String capitalEscolhida = "Salvador";
+        capitalEscolhida = capitalEscolhida.toLowerCase(Locale.ROOT);
+        if (capitalEscolhida == this.capitalAtual) {
+            this.pontos++;
+        } else {
+            // Informar o usuário que ele errou e a capital correta
+        }
+
+        if (this.rodada < 4) { // Verificando se é a última rodada do jogo
+            this.rodada++;
+        } else {
+            // Mostrar quantos pontos o usuário fez através do atributo "this.pontos"
+            // Desabilitar os dois botões
+            // (Opcional) Opção para o usuário reiniciar o jogo
+        }
     }
 
 }
